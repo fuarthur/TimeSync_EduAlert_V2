045d7886bcafb890fb0b8c7482145ea390aba85a
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2024-09-11 15:38:41 -0500

    chore: update minSDK from 24 to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ams.timesyncedualertv2"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
