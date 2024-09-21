92cfa22cf3fa6d774e07b07c88f38bbe45addd87
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2024-09-20 23:38:12 -0500

    chore: upgrade minSDK to 33
    (cherry picked from commit 9b9a72617fe4c203c1532285660fd375af9d8fa9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ams.timesyncedualertv2"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
