94c568932b173d2a7b522a90f9ea15ef3edc3f54
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2024-09-23 14:46:16 -0500

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.work.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
